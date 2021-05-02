33f274a206e16b1a7eb84ea1e772492055f13488
vitor-apolinario <vitor-apolinario@github.com>
2021-05-02 10:05:14 -0300

    feat: file management utils

---
+++ src/utils/fileUtils.ts
@@ -0,0 +1,24 @@
+import { dir } from 'console';
+import { writeFileSync, existsSync } from 'fs';
+import { join } from 'path';
+
+const makeDir = require('make-dir');
+
+
+class FileUtils {
+  static createDir(dirPath: string[]): string {
+    const joinedPath: string = join(...dirPath);
+        
+    if(existsSync(joinedPath)) {
+      return joinedPath;
+    }
+    
+    return makeDir.sync(joinedPath);
+  }
+
+  static writeFile(fileBuffer: Buffer, filePath: string[], fileName: string) {    
+    const existantPath: string = this.createDir(filePath);
+
+    writeFileSync(join(existantPath, fileName), fileBuffer)
+  }
+}
